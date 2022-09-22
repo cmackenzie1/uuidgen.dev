e638b3411f896169515f4a536d7183c5a7ecaa81
Cole MacKenzie <colemackenzie1@gmail.com>
2022-09-21 21:41:58 -0700

    add analytics engine

--- workers/src/index.ts
+++ workers/src/index.ts
@@ -1,5 +1,7 @@
 import { Router } from 'itty-router';
 import { CORS_HEADERS } from './constants';
+import { AnalyticsEngine, AppEnv } from './types/env';
+import { ErrorObject, serializeError } from 'serialize-error';
 
 const router = Router();
 
@@ -13,11 +15,17 @@ router.options('*', () => {
   });
 });
 
-router.get('/', (request: Request) => {
-  return new Response(crypto.randomUUID(), { headers: { ...CORS_HEADERS } });
+router.head('/', () => new Response());
+router.get('/', (request: Request, env: AppEnv) => {
+  env.stats.writeDataPoint({ doubles: [1] });
+  return new Response(crypto.randomUUID(), {
+    status: 200,
+    headers: { ...CORS_HEADERS },
+  });
 });
 
-router.get('/bulk', (request: Request) => {
+router.head('/bulk', () => new Response());
+router.get('/bulk', (request: Request, env: AppEnv) => {
   const { searchParams } = new URL(request.url);
   const count: number = parseInt(
     searchParams.get('n') ||
@@ -29,9 +37,81 @@ router.get('/bulk', (request: Request) => {
   const data = Array(count)
     .fill(0)
     .map(() => crypto.randomUUID());
-  return new Response(`${data.join('\n')}`, { headers: { ...CORS_HEADERS } });
+  env.stats.writeDataPoint({ doubles: [count] });
+  return new Response(`${data.join('\n')}`, {
+    status: 200,
+    headers: { ...CORS_HEADERS },
+  });
+});
+
+router.all('*', (request: Request, env: AppEnv) => {
+  const { url, method } = request;
+  env.logs.writeDataPoint({
+    blobs: [
+      'NotFound',
+      `${request.method} ${request.url}`,
+      '',
+      '',
+      JSON.stringify({
+        url,
+        method,
+        headers: Object.fromEntries(request.headers),
+      }),
+    ],
+  });
+  return new Response('Not Found.', { status: 404 });
 });
 
 export default {
-  fetch: router.handle, // yep, it's this easy.
+  async fetch(request: Request, env: AppEnv, ctx: ExecutionContext) {
+    try {
+      const resp = router.handle(request, env, ctx);
+      checkResponse(resp);
+      logRequest(env, request, resp);
+      return resp;
+    } catch (e) {
+      const err: ErrorObject = serializeError(e);
+      const resp = new Response('Internal Server Error.', { status: 500 });
+      logRequest(env, request, resp, err);
+      return resp;
+    }
+  },
+};
+
+const checkResponse = (resp: Response | undefined) => {
+  if (resp === undefined) throw Error('UndefinedResponse');
+};
+
+const logRequest = (
+  env: AppEnv,
+  request: Request,
+  response: Response,
+  err?: ErrorObject,
+) => {
+  const { method, url, cf } = request;
+  const { status, statusText } = response;
+  env.requests.writeDataPoint({
+    blobs: [
+      method,
+      url,
+      statusText,
+      cf?.colo || '',
+      cf?.country || '',
+      cf?.region || '',
+      cf?.city || '',
+      cf?.latitude || '',
+      cf?.longitude || '',
+    ],
+    doubles: [status, cf?.clientTcpRtt || 0],
+  });
+  if (err)
+    env.logs.writeDataPoint({
+      blobs: [
+        err?.name || 'unknown',
+        err?.message || '',
+        err?.code || '',
+        err?.stack || '',
+        JSON.stringify(err),
+      ],
+    });
 };
--- workers/src/types/env.d.ts
+++ workers/src/types/env.d.ts
@@ -1,3 +1,16 @@
+interface AnalyticsEngine {
+  writeDataPoint(event?: AnalyticsEngineEvent): void;
+}
+
+interface AnalyticsEngineEvent {
+  version?: any;
+  doubles?: number[];
+  blobs?: (ArrayBuffer | string | null)[];
+}
+
 export interface AppEnv {
+  requests: AnalyticsEngine;
+  stats: AnalyticsEngine;
+  logs: AnalyticsEngine;
   DURABLE_COUNTER: DurableObjectNamespace;
 }
