7c1545e158071d65434947d6752052b204fb8f85
Cole MacKenzie <colemackenzie1@gmail.com>
2024-11-17 20:28:28 -0800

    feat: update footer

--- app/src/components/Footer.tsx
+++ app/src/components/Footer.tsx
@@ -1,18 +1,17 @@
-import { ActionIcon, Box, Button, Center, Group, Text } from '@mantine/core';
+import { Anchor, Center, Group, Text } from '@mantine/core';
 import { IconBrandGithub } from '@tabler/icons-react';
+import Link from 'next/link';
 
 export default function Footer() {
   return (
     <Group wrap="nowrap" align="center" justify="space-evenly" h="70px" p="md">
       <Center>
-        <Box>
-          <Button variant="link" color="gray" size="xs" component="a" href="https://github.com/cmackenzie1/uuidgen.dev">
-            <ActionIcon size="xs" style={{ marginRight: 5 }}>
-              <IconBrandGithub />
-            </ActionIcon>
-            <Text size="xs">GitHub</Text>
-          </Button>
-        </Box>
+        <Anchor href="https://github.com/cmackenzie1/uuidgen.dev" target="_blank" component={Link} underline="never">
+          <Group gap="xs">
+            <Text c={'dimmed'}>A project by @cmackenzie1</Text>
+            <IconBrandGithub stroke={1.5} />
+          </Group>
+        </Anchor>
       </Center>
     </Group>
   );
